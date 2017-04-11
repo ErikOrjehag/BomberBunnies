4729de967be3ba55e19b4fe768954fc28e649b0b
RolfSievert <rolsi701@student.liu.se>
2017-04-11 17:52:06 +0200

    broke joy

--- CPU.vhd
+++ CPU.vhd
@@ -42,7 +42,7 @@ architecture behavioral of CPU is
   component PROGRAM_MEMORY
     port (
       pAddr : in  unsigned(11 downto 0);
-      pData : out std_logic_vector(21 downto 0));
+      pData : out std_logic_vector(22 downto 0));
   end component;
 
   -- Micro memory component
@@ -52,23 +52,31 @@ architecture behavioral of CPU is
       uData : out std_logic_vector(28 downto 0));
   end component;
 
-  signal PM : std_logic_vector(21 downto 0);
+  signal PM : std_logic_vector(22 downto 0);
   signal uPM : std_logic_vector(28 downto 0);
 
   -- Bus
-  signal buss : std_logic_vector(21 downto 0) := (others => '0');
+  signal buss : std_logic_vector(22 downto 0) := (others => '0');
 
   -- GRx
-  type grx_t is array (0 to 7) of std_logic_vector(21 downto 0);
+  type grx_t is array (0 to 15) of std_logic_vector(22 downto 0);
   signal GRx : grx_t := (
-    "0000000000000000000000",
-    "0000000000000000000000",
-    "0000000000000000000000",
-    "0000000000000000000000",
-    "0000000000000000000000",
-    "0000000000000000000000",
-    "0000000000000000000000",
-    "0000000000000001000000"
+    "00000000000000000000000", -- 0
+    "00000000000000000000000", -- 1
+    "00000000000000000000000", -- 2
+    "00000000000000000000000", -- 3*
+    "00000000000000000000000", -- 4
+    "00000000000000000000000", -- 5
+    "00000000000000000000000", -- joy1x
+    "00000000000000000000000", -- joy1y
+    "00000000000000000000000", -- btn1
+    "00000000000000000000000", -- joy2x
+    "00000000000000000000000", -- joy2y
+    "00000000000000000000000", -- btn2
+    "00000000000000000000000", -- p1x
+    "00000000000000000000000", -- p1y
+    "00000000000000000000000", -- p2x
+    "00000000000000001000000"  -- p2y
   );
   signal GRx_x  : integer := 0;
 
@@ -119,16 +127,16 @@ architecture behavioral of CPU is
   -- IR signals
   signal ir_addr : std_logic_vector(11 downto 0);
   signal ir_m : std_logic_vector(1 downto 0);
-  signal ir_grx : std_logic_vector(2 downto 0);
+  signal ir_grx : std_logic_vector(3 downto 0);
   signal ir_op : std_logic_vector(4 downto 0);
   
   -- Registers
-  signal IR : std_logic_vector(21 downto 0) := (others => '0');
+  signal IR : std_logic_vector(22 downto 0) := (others => '0');
   signal AR : std_logic_vector(11 downto 0) := (others => '0');
 --  signal PC : std_logic_vector(11 downto 0) := (0 => '1', 1 => '1', others => '0');
   signal PC : std_logic_vector(11 downto 0) := (others => '0');
   signal ASR : unsigned(11 downto 0) := (others => '0');
-  signal LC : std_logic_vector(21 downto 0) := (others => '0');  --fit uAddr
+  signal LC : std_logic_vector(22 downto 0) := (others => '0');
   signal uPC : unsigned(8 downto 0) := (others => '0');
 
   -- Flags
@@ -141,11 +149,21 @@ architecture behavioral of CPU is
 
 begin  -- behavioral
 
+  -- Joystick 1
+  GRx(6)(9 downto 0) <= joy1x;
+  GRx(7)(9 downto 0) <= joy1y;
+  GRx(8)(0) <= btn1;
+
+  -- Joystick 2
+  GRx(9)(9 downto 0) <= joy2x;
+  GRx(10)(9 downto 0) <= joy2y;
+  GRx(11)(0) <= btn2;
+  
   -- Player positions
-  p1x <= GRx(4)(9 downto 0);
-  p1y <= GRx(5)(9 downto 0);
-  p2x <= GRx(6)(9 downto 0);
-  p2y <= GRx(7)(9 downto 0);
+  p1x <= GRx(12)(9 downto 0);
+  p1y <= GRx(13)(9 downto 0);
+  p2x <= GRx(14)(9 downto 0);
+  p2y <= GRx(15)(9 downto 0);
 
   -- uPM signals
   upm_instr <= uPM;
@@ -162,28 +180,28 @@ begin  -- behavioral
   -- IR signals
   ir_addr   <= IR(11 downto 0);
   ir_m      <= IR(13 downto 12);
-  ir_grx    <= IR(16 downto 14);
-  ir_op     <= IR(21 downto 17);
+  ir_grx    <= IR(17 downto 14);
+  ir_op     <= IR(22 downto 18);
   
   -- TB
   GRx_x <= to_integer(unsigned(ir_grx)) when (upm_s = "0") else to_integer(unsigned(ir_m));
 
   with upm_tb select buss <=
-    "0000000000" & std_logic_vector(ASR) when "0001", 
+    "00000000000" & std_logic_vector(ASR) when "0001", 
     IR                                   when "0010",
     PM                                   when "0011",
-    "0000000000" & PC                    when "0100",
+    "00000000000" & PC                    when "0100",
     GRx(GRx_x)                           when "0101",
-    "0000000000" & AR                    when "0110",
+    "00000000000" & AR                    when "0110",
                                             --"0111" --ledig
-    x"000" & "00" & tileTypeRead         when "1000",
-    x"000" & "00" & tilePointer          when "1001",
-    x"000" & joy1x                       when "1010",
-    x"000" & joy1y                       when "1011",
-    x"00000" & "0" & btn1                when "1100",
-    x"000" & joy2x                       when "1101",
-    x"000" & joy2y                       when "1110",
-    x"00000" & "0" & btn1                when "1111",
+    x"000" & "000" & tileTypeRead         when "1000",
+    x"000" & "000" & tilePointer          when "1001",
+--    x"000" & joy1x                       when "1010",
+--    x"000" & joy1y                       when "1011",
+--    x"00000" & "0" & btn1                when "1100",
+--    x"000" & joy2x                       when "1101",
+--    x"000" & joy2y                       when "1110",
+--    x"00000" & "0" & btn1                when "1111",
     buss                                 when others;
 
   L <= '0' when LC = (LC'range => '0') else '1';
@@ -204,12 +222,12 @@ begin  -- behavioral
         when "0111" => null;            --ledig
         when "1000" => tileTypeWrite <= buss(7 downto 0);
         when "1001" => tilePointer <= buss(7 downto 0);
-        when "1010" => null;            --joy1x
-        when "1011" => null;            --joy1y
-        when "1100" => null;            --btn1
-        when "1101" => null;            --joy2x
-        when "1110" => null;            --joy2y
-        when "1111" => null;            --btn2
+--        when "1010" => null;            --joy1x
+--        when "1011" => null;            --joy1y
+--        when "1100" => null;            --btn1
+--        when "1101" => null;            --joy2x
+--        when "1110" => null;            --joy2y
+--        when "1111" => null;            --btn2
         when others => null;
       end case;
 
@@ -257,7 +275,7 @@ begin  -- behavioral
         when "00" => null;
         when "01" => LC <= std_logic_vector(unsigned(LC) - 1);
         when "10" => LC <= buss;
-        when "11" => LC <= "0000000000000" & upm_uaddr;
+        when "11" => LC <= "00000000000000" & upm_uaddr;
         when others => null;
       end case;
 
