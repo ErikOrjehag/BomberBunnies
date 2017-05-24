5d625beefda93260ee8797c23c72f53578598fa4
RolfSievert <rolsi701@student.liu.se>
2017-05-24 14:29:04 +0200

    Cleaned up for packing

--- PROGRAM_MEMORY.vhd
+++ PROGRAM_MEMORY.vhd
@@ -16,48 +16,6 @@ end PROGRAM_MEMORY;
 
 architecture Behavioral of PROGRAM_MEMORY is
 
-  -- Add names for different operations that are binary?
-  -- operations
-  constant load : std_logic_vector        := "00000";
-  constant store : std_logic_vector       := "00001";
-  constant add : std_logic_vector         := "00010";
-  constant sub : std_logic_vector         := "00011";
-  constant jump : std_logic_vector        := "00100";
-  constant sleep : std_logic_vector       := "00101";
-  constant beq : std_logic_vector         := "00110";
-  constant bne : std_logic_vector         := "00111";
-  constant tileWrite : std_logic_vector   := "01110";
-  constant tileRead : std_logic_vector    := "01111";
-  constant tilePointer : std_logic_vector := "10000";
-  constant joy1r : std_logic_vector       := "10001";
-  constant joy1u : std_logic_vector       := "10010";
-  constant joy1l : std_logic_vector       := "10011";
-  constant joy1d : std_logic_vector       := "10100";
-  constant btn1 : std_logic_vector        := "10101";
-  constant joy2r : std_logic_vector       := "10110";
-  constant joy2u : std_logic_vector       := "10111";
-  constant joy2l : std_logic_vector       := "11000";
-  constant joy2d : std_logic_vector       := "11001";
-  constant btn2 : std_logic_vector        := "11010";
-
-  -- GRx
-  constant gr0 : std_logic_vector  := "0000";
-  constant gr1 : std_logic_vector  := "0001";
-  constant gr2 : std_logic_vector  := "0010";
-  constant gr3 : std_logic_vector  := "0011";
-  constant gr4 : std_logic_vector  := "0100";
-  constant gr5 : std_logic_vector  := "0101";
-  constant gr6 : std_logic_vector  := "0110";
-  constant gr7 : std_logic_vector  := "0111";
-  constant gr8 : std_logic_vector  := "1000";
-  constant gr9 : std_logic_vector  := "1001";
-  constant gr10 : std_logic_vector := "1010";
-  constant gr11 : std_logic_vector := "1011";
-  constant gr12 : std_logic_vector := "1100";
-  constant gr13 : std_logic_vector := "1101";
-  constant gr14 : std_logic_vector := "1110";
-  constant gr15 : std_logic_vector := "1111";
-
   -- Program memory
   type pm_t is array (0 to 2047) of std_logic_vector(22 downto 0);  --2047
   constant pm_c : pm_t := (
